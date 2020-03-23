080b1c627aa6323302b8cf52796bb2e91813df28
PavloPantus <mr.pantus@gmail.com>
2020-03-23 02:04:16 +0200

    Updates

--- precache-manifest.c21948a54aae8e36481260aa60f416e9.js
+++ precache-manifest.c21948a54aae8e36481260aa60f416e9.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "baa64fab6e5810450650f4e2fe0608a5",
+    "revision": "0c94758bce8fc57325835ad8822ecf18",
     "url": "/react_battles/index.html"
   },
   {
-    "revision": "a86672b99d6d3db3d4de",
-    "url": "/react_battles/static/css/main.7e6e1b06.chunk.css"
+    "revision": "903194668d1693edc1eb",
+    "url": "/react_battles/static/css/main.1dde53b7.chunk.css"
   },
   {
     "revision": "3697ef11eeceb2259cca",
     "url": "/react_battles/static/js/2.265da307.chunk.js"
   },
   {
-    "revision": "a86672b99d6d3db3d4de",
-    "url": "/react_battles/static/js/main.38591580.chunk.js"
+    "revision": "903194668d1693edc1eb",
+    "url": "/react_battles/static/js/main.cd8c4bc7.chunk.js"
   },
   {
     "revision": "f3d7f95fb093b3467193",
